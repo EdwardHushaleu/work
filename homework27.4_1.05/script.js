303420702c0311ee3e0560848ee4179261f6bc1b
EdwardHushaleu <2779522@mail.com>
2020-04-27 19:52:55 +0300

    Delete an element from the array (in two ways)

--- homework27.4_1.05/script.js
+++ homework27.4_1.05/script.js
@@ -1,7 +1,16 @@
-var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest' ];
+var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest']; //Добавляем элемент в начало массива
 cityArray.unshift('Minsk');
-console.log(cityArray)
+console.log(cityArray);
 
-var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest' ];
+var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest']; //Добавляем элемент в конец массива
 cityArray.push('Minsk');
 console.log(cityArray);
+
+var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest', 'Minsk'];   //
+cityArray.pop();
+console.log(cityArray);                                                           //Удалить элемент из массива (двумя способами)
+
+var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest', 'Minsk'];   
+cityArray.shift();
+console.log(cityArray);                                                        //       
+
