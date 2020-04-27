e945a285ea47ab23ae4df055212e0615034f0a52
EdwardHushaleu <2779522@mail.com>
2020-04-27 19:43:23 +0300

    Added a word to the end of the array

--- homework27.4_1.05/script.js
+++ homework27.4_1.05/script.js
@@ -1,3 +1,7 @@
 var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest' ];
 cityArray.unshift('Minsk');
 console.log(cityArray)
+
+var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest' ];
+cityArray.push('Minsk');
+console.log(cityArray);
