99bd8704e074b71dabafac1eb2da4335cfbc0b3d
EdwardHushaleu <2779522@mail.com>
2020-04-28 18:57:44 +0300

    Redid the fourth task and did the fifth

--- homework27.4_1.05/script.js
+++ homework27.4_1.05/script.js
@@ -1,21 +1,37 @@
-var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest']; //Добавляем элемент в начало массива
+//1)Добавляем элемент в начало массива
+var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest']; 
 cityArray.unshift('Minsk');
 console.log(cityArray);
-
-var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest']; //Добавляем элемент в конец массива
+//2)Добавляем элемент в конец массива
+var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest']; 
 cityArray.push('Minsk');
 console.log(cityArray);
-
-var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest', 'Minsk'];   //
+//3)Удаляем элемент из массива (двумя способами)
+var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest', 'Minsk'];   
 cityArray.pop();
-console.log(cityArray);                                                           //Удаляем элемент из массива (двумя способами)
+console.log(cityArray);                                                           
 
 var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest', 'Minsk'];   
 cityArray.shift();
-console.log(cityArray);                                                        //       
+console.log(cityArray);                                                             
+// 4)Удалили повторяющиеся значения из массива---????&????????
+var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Hrodno', 'Mogilev', 'Brest', 'Minsk', 'Minsk'];
+  result = [];
+  for (var i = 0; i < cityArray.length; i++) {
+      if(cityArray[i + 1] != cityArray[i]){
+        result.push(cityArray[i])
+      }
+    }
+  console.log(result);
+//5) 
+var max = 7;
+ var valueArray = [ 25, 1, 34, 'Homel', 2, 3, 51, 8,];
+ for (var i = 0; i < valueArray.length; i++ ){
+   if ( valueArray[i] > max){
+    console.log(valueArray[i]);
+   }
+ };
+ //6)
 
-var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Hrodno', 'Mogilev', 'Brest', 'Minsk', 'Minsk']; // Удалили повторяющиеся значения из массива---????&????????
-var newcityArray = cityArray.filter(function(elem, pos) {
-  return cityArray.indexOf(elem) == pos;                                                              
-});
-console.log(newcityArray);
+
+ 
