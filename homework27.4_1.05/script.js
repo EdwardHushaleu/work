88ac4e4d70aa26d451f1c9d237c2a62e0e50fbb3
EdwardHushaleu <2779522@mail.com>
2020-04-30 19:48:59 +0300

    Made changes in the second, third, fourth task

--- homework27.4_1.05/script.js
+++ homework27.4_1.05/script.js
@@ -1,19 +1,23 @@
+
 //1)Добавляем элемент в начало массива
 var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest']; 
 cityArray.unshift('Minsk');
 console.log(cityArray);
+
 //2)Добавляем элемент в конец массива
 var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest']; 
 cityArray.push('Minsk');
 console.log(cityArray);
+
 //3)Удаляем элемент из массива (двумя способами)
 var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest', 'Minsk'];   
-cityArray.pop();
-console.log(cityArray);                                                           
+delete cityArray [2];
+console.log(cityArray);                                                                                                                    
 
 var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Mogilev', 'Brest', 'Minsk'];   
-cityArray.shift();
-console.log(cityArray);                                                             
+cityArray.splice(2,1);
+console.log(cityArray);                                                              
+
 // 4)Удалили повторяющиеся значения из массива---????&????????
 var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Hrodno', 'Mogilev', 'Brest', 'Minsk', 'Minsk'];
   result = [];
@@ -23,22 +27,26 @@ var cityArray = ['Homel', 'Hrodno', 'Vitebsk', 'Hrodno', 'Mogilev', 'Brest', 'Mi
       }
     }
   console.log(result);
-//5) 
-var max = 7;
- var valueArray = [ 25, 1, 34, 'Homel', 2, 3, 51, 8,];
- for (var i = 0; i < valueArray.length; i++ ){
-   if ( valueArray[i] > max){
+
+//5)  Выводим в консоль все числовые значения больше 7.
+var valueArray = [ 25, 1, 34, 'Homel', 2, 3, 51, 8];
+for (var i = 0; i < valueArray.length; i++ ){
+  if(valueArray[i] > 7){
     console.log(valueArray[i]);
-   }
- };
- //6
+    console.log(typeof(valueArray[i]))
+  }
+};
+
+ //6)Выводим в консоль все строковые значения массива, длина которых больше 5.
  var cityArray = ['Homell', 'Hrodno', 5, 'Vitebsk', 'Mogi', 4 , 'Brest', 'Minsk'];
 for (var i=0; i < cityArray.length; i++){
   if (cityArray[i].length > 5){
-    console.log(cityArray[i].length); 
+    console.log(cityArray[i]);
+    console.log(typeof(cityArray[i])); 
   }
 };
- //11)
+ 
+//11)Перебираем массив и выводим в консоль сообщение на каждую итерацию о том чётное перед нами число, либо нечётное
  var valueArray = [ 25, 1, 34, 2, 3, 51, 8, 100];
  for (var i = 0; i < valueArray.length; i++){
   if( valueArray[i] % 2 === 0){
@@ -47,8 +55,8 @@ for (var i=0; i < cityArray.length; i++){
     console.log('odd');
   }
 };
-//10)
 
+//10)Перебираем массив и выводим только те элементы, в которых содержится буква 'u'
 var cityArray = ['Homuel', 'Hrodno', 'Vitebsuk', 'Mogilev', 'Breust', 'Minsk'];
 var result = [];
 for( var i = 0; i < cityArray.length; i++){
