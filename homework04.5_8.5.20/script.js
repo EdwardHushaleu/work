6d15fc55829bf765a53f1e1bb41eeb4e7897144a
EdwardHushaleu <2779522@mail.com>
2020-05-07 00:37:51 +0300

    even & odd number

--- homework04.5_8.5.20/script.js
+++ homework04.5_8.5.20/script.js
@@ -1,4 +1,12 @@
+var textItem = document.getElementsByClassName('l-tasks__item-text');
 
-
- 
- 
+var fun = function(num){
+    if(typeof num === 'number' && num % 2 === 0){
+        textItem[0].innerText = 'Number'+ ' ' + num + ' ' +'is even'
+    }
+    else (textItem[0].innerText = 'Number'+ ' ' + num + ' ' +'is odd')
+      if( typeof num === 'undefined'){
+        textItem[0].innerText = 'This is not a number'
+      }  
+};
+fun(8);
