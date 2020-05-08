4ad141706e829542ee12a391fcaec5aedbe61887
EdwardHushaleu <2779522@mail.com>
2020-05-08 19:49:04 +0300

    remade span removal

--- homework04.5_8.5.20/script.js
+++ homework04.5_8.5.20/script.js
@@ -5,37 +5,28 @@ var buttonRemove = document.querySelector('.l-tasks__btn._remove');
 var ul = document.querySelector('.l-tasks');
 var arraySpan = document.getElementsByClassName('l-tasks__span');
 var removeSpan;
-var spansState = { 
-    isFirstSpanDelited: false,
-    isSecondSpanDelited: false, 
-}
 
 var fun = function(num){
     if(typeof num === 'number' && num % 2 === 0){
         textItem[0].innerText = 'Number'+ ' ' + num + ' ' +'is even'
     } else{ 
         textItem[0].innerText = 'Number'+ ' ' + num + ' ' +'is odd';
-} 
+};
 };
 var click = function (){
  body.classList.toggle('_color-scheme-light');
- ul.classList.toggle('l-tasks');
- buttonToggle.classList.toggle('l-tasks__btn')
- buttonRemove.classList.toggle('l-tasks__btn')
 };
 
 fun(4);
 buttonToggle.addEventListener('click', click);
 
 removeSpan = function(){
-    if(body.classList.contains('_color-scheme-light') && !spansState.isFirstSpanDelited ){
+    if(body.classList.contains('_color-scheme-light')){
         arraySpan[0].remove();
-        spansState.isFirstSpanDelited = true;
-    } 
-    if(!body.classList.contains('_color-scheme-light') && !spansState.isSecondSpanDelited ){
-        arraySpan[arraySpan.length-1].remove();
-        spansState.isSecondSpanDelited = true;
-    } 
+    } else {
+        arraySpan[arraySpan.length-1].remove()
+    };
+    buttonRemove.removeEventListener('click', removeSpan);    
 };
 buttonRemove.addEventListener('click', removeSpan);
 
