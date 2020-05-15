a8d9df65f54f1b629307ff4125405136211f76ad
EdwardHushaleu <2779522@mail.com>
2020-05-15 18:04:10 +0300

    fix js

--- homework11.05_15.05/script.js
+++ homework11.05_15.05/script.js
@@ -2,32 +2,34 @@ var bUnivers = document.querySelector('.b-univers');
 var bUniversPlanet = document.querySelector('.b-univers__img');
 
 bUnivers.addEventListener('click', function(e) {
-    var x = e.offsetX - bUniversPlanet.offsetWidth / 2;
-    var y = e.offsetY - bUniversPlanet.offsetHeight / 2;
+  var x = e.offsetX - bUniversPlanet.offsetWidth / 2;
+  var y = e.offsetY - bUniversPlanet.offsetHeight / 2;
    
-    if (e.target === bUniversPlanet ) {
-        return bUniversPlanet.style.display = 'none';
-    } else if (bUniversPlanet.style.display === 'none'){
-        return bUniversPlanet.style.display = '';   
-    };
+  if (e.target === bUniversPlanet ) {
+    return bUniversPlanet.style.display = 'none';
+  };
 
-    if (x < 0) {
-        x = 0; 
-    };
+  if (bUniversPlanet.style.display === 'none'){
+    return bUniversPlanet.style.display = '';   
+  };
 
-    if (x + bUniversPlanet.offsetWidth > bUnivers.offsetWidth) {
-        x = bUnivers.offsetWidth - bUniversPlanet.offsetWidth;  
-    };
+  if (x < 0) {
+    x = 0; 
+  };
 
-    if (y < 0) {
-        y = 0;
-    };
+  if (x + bUniversPlanet.offsetWidth > bUnivers.offsetWidth) {
+    x = bUnivers.offsetWidth - bUniversPlanet.offsetWidth;
+  };
 
-    if (y + bUniversPlanet.offsetHeight > bUnivers.offsetHeight) {
-        y = bUnivers.offsetHeight - bUniversPlanet.offsetHeight;
-    };
+  if (y < 0) {
+    y = 0;
+  };
+
+  if (y + bUniversPlanet.offsetHeight > bUnivers.offsetHeight) {
+    y = bUnivers.offsetHeight - bUniversPlanet.offsetHeight;
+  };
     
-    bUniversPlanet.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+  bUniversPlanet.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 
 });
 
