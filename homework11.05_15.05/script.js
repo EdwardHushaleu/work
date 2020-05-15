5a41cf1627a131c0740dc6c3027a1dc12b9a578c
EdwardHushaleu <2779522@mail.com>
2020-05-15 17:11:45 +0300

    fix js

--- homework11.05_15.05/script.js
+++ homework11.05_15.05/script.js
@@ -2,11 +2,9 @@ var bUnivers = document.querySelector('.b-univers');
 var bUniversPlanet = document.querySelector('.b-univers__img');
 
 bUnivers.addEventListener('click', function(e) {
-  
     var x = e.offsetX - bUniversPlanet.offsetWidth / 2;
     var y = e.offsetY - bUniversPlanet.offsetHeight / 2;
    
-    
     if (e.target === bUniversPlanet ) {
         return bUniversPlanet.style.display = 'none';
     } else if (bUniversPlanet.style.display === 'none'){
@@ -15,18 +13,21 @@ bUnivers.addEventListener('click', function(e) {
 
     if (x < 0) {
         x = 0; 
-    } else if (x + bUniversPlanet.offsetWidth > bUnivers.offsetWidth) {
+    };
+
+    if (x + bUniversPlanet.offsetWidth > bUnivers.offsetWidth) {
         x = bUnivers.offsetWidth - bUniversPlanet.offsetWidth;  
     };
 
     if (y < 0) {
         y = 0;
-    } else if (y + bUniversPlanet.offsetHeighths > bUnivers.offsetHeight) {
+    };
+
+    if (y + bUniversPlanet.offsetHeight > bUnivers.offsetHeight) {
         y = bUnivers.offsetHeight - bUniversPlanet.offsetHeight;
     };
     
- bUniversPlanet.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+    bUniversPlanet.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 
-    console.log(x, y);
 });
 
