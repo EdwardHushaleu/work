1f59e0f0a9f53dde2c3faace3ca4420fb8cf5374
EdwardHushaleu <2779522@mail.com>
2020-05-16 18:56:22 +0300

    form

---
+++ homework16.5.2020/script.js
@@ -0,0 +1,33 @@
+var body = document.body;
+var form = document.querySelector('.f-default');
+var button = document.querySelector('.f-default__btn');
+
+button.addEventListener('click', function(e){
+  e.stopPropagation();
+});
+
+form.addEventListener('submit', function(e){
+e.preventDefault();
+
+  var valueImail = e.target.elements.email.value;
+  var valuePassword = e.target.elements.password.value
+
+  if(!valueImail || !valuePassword){
+  alert('Дружище, поля-то должны быть заполнены');
+} else if (valuePassword && valuePassword.length < 10){
+  alert('Капец пароль маленький, нужно больше циферок или букавок');
+};
+});
+
+body.addEventListener('click', function(e){
+  e.stopImmediatePropagation()
+  console.log("BODY_1");
+});
+
+body.addEventListener('click', function(){
+  console.log("BODY_2");
+});
+
+body.addEventListener('click', function(){
+  console.log("BODY_3");
+});
