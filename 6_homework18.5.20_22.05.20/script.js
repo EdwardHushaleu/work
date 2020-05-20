248ea641f36e2d021de8cb675342dc61140482d5
EdwardHushaleu <2779522@mail.com>
2020-05-20 14:03:27 +0300

    work 6 find an array

---
+++ 6_homework18.5.20_22.05.20/script.js
@@ -0,0 +1,23 @@
+var body = document.body;
+var buttonColor = document.querySelector('button');
+var color = ['#ffffff'];
+var obj = {
+  field1: 'Hello',
+  field2: true,
+  field3: ['#00bcd4', '#ffc107', '#2196f3'],
+  field4: 575,
+  field5: true,
+};
+
+buttonColor.addEventListener('click', function() {
+  for(var prop in obj){
+    if(Array.isArray(obj[prop])){
+      color.push(...obj[prop]);
+      console.log(color);
+    }
+  }
+
+})
+console.log(color);
+
+
