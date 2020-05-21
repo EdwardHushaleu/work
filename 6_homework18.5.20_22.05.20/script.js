f71277149eefe2a9feabf93208828c1d92df1d88
EdwardHushaleu <2779522@mail.com>
2020-05-21 13:35:14 +0300

    added keyboard action

--- 6_homework18.5.20_22.05.20/script.js
+++ 6_homework18.5.20_22.05.20/script.js
@@ -1,6 +1,6 @@
 var body = document.body;
-var buttonColor = document.querySelector('button');
-var color = [];
+var buttonColors = document.querySelector('button');
+var colors = [];
 var obj = {
   field1: 'Hello',
   field2: true,
@@ -8,25 +8,53 @@ var obj = {
   field4: 575,
   field5: true,
 };
+var counter;
 
-buttonColor.addEventListener('click', function() {
 
-  var randomColors = Math.floor(Math.random() * color.length-1);
+buttonColors.addEventListener('click', function(){
   
-  for(var prop in obj){
+  if(!colors.length){
+    for(var prop in obj){
     if(Array.isArray(obj[prop])){
-      color.push(...obj[prop]);
+    colors = obj[prop]
+    };
     };
   };
-  
-  body.style.backgroundColor = color[randomColors];
+
+  counter = Math.floor(Math.random() * colors.length);
+
+  body.style.backgroundColor = colors[counter];
 });
 
-document.addEventListener('keydown', function(e){
-  
-  if(e.keyCode === 39){
-    
-  }
+body.addEventListener('keyup', function(e){
 
-})
+  var isRightKey = e.keyCode === 39;
+  var isLeftKey = e.keyCode === 37;
+  
+  if(isRightKey){
+    if(counter === colors.length){
+      counter = 0;
+    } else if (counter >= 0) {
+      counter++;
+    } else {
+    alert('Достаньте массив цветов!');
+    }
+
+    body.style.backgroundColor = colors[counter];
+  } 
+  
+  else if(isLeftKey){
+    if(counter === 0){
+      counter = colors.length;
+    } else if (counter <= colors.length) {
+      counter--;
+    } else {
+    alert('Достаньте массив цветов!');
+    }
+
+    body.style.backgroundColor = colors[counter];
+  };  
+});
+
+
 
