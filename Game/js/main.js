1a0d09b000cc02a65b8ed9c45bb4bb00e3cea6f1
EdwardHushaleu <2779522@mail.com>
2020-05-26 16:32:24 +0300

    motion calculation correction aim

--- Game/js/main.js
+++ Game/js/main.js
@@ -6,32 +6,31 @@ var bShooterImgAim = document.querySelector('.b-shooter__img-aim');
 bShooter.addEventListener('click', function(e){
      var x = e.offsetX - bShooterAim.offsetWidth / 2;
      var y = e.offsetY - bShooterAim.offsetHeight / 2;
+     var limitX = bShooter.offsetWidth - bShooterAim.offsetWidth;
+     var limitY = bShooter.offsetHeight - bShooterAim.offsetHeight;
 
     if(x < 0){
         x = 0
-    };
-
-    if(x + bShooterAim.offsetWidth > bShooter.offsetWidth){
-        x = bShooter.offsetWidth - bShooterAim.offsetWidth;
+    } else if(x > limitX){
+        x = limitX;
     };
 
     if(y < 0){
         y = 0
+    } else if(y > limitY){
+        y = limitY;
     };
 
-    if(y + bShooterAim.offsetHeight > bShooter.offsetHeight){
-        y = bShooter.offsetHeight - bShooterAim.offsetHeight;
-    };
-
-    bShooterAim.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+bShooterAim.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 });
 
 
 body.addEventListener('keydown', function(e){
     
     if(e.keyCode === 13){
-        bShooterImgAim.style.transform ='scale(.7)';
+        bShooterImgAim.style.transform ='scale(.9)';
     }
+    
 });
 
 body.addEventListener('keyup', function(e){
