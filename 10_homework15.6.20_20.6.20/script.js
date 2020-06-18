8196c71a444d367c8dd8fa06a2e37df441903b93
EdwardHushaleu <2779522@mail.com>
2020-06-18 19:25:33 +0300

    fix homework

--- 10_homework15.6.20_20.6.20/script.js
+++ 10_homework15.6.20_20.6.20/script.js
@@ -2,10 +2,10 @@
 const errorHendler = (errorE) => {console.log(errorE.message)};
 let arr =[];
 let arr2 =[];
-const requestForUsers = function (seccess, error){
+const requestForUsers = function (seccess, error, url){
   const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'https://randomuser.me/api/?results=3&gender=male');
+  xhr.open('GET', 'https://randomuser.me/api/?results=3&gender' + url);
   xhr.onreadystatechange = function(){
     if(xhr.readyState === 4){
       if(xhr.status >= 200 && xhr.status < 300) {
@@ -20,30 +20,18 @@ const requestForUsers = function (seccess, error){
     }
   };
 
-  xhr.open('GET','https://randomuser.me/api/?results=3&gender=female');
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
-      if(xhr.status >= 200 && xhr.status < 300){
-        seccess(JSON.parse(xhr.response))
-      } else {
-        error({
-          message: 'Error',
-        });
-      }
-    }
-  };
-
   xhr.send();
 };
 
 requestForUsers(function(male){
   arr.push(male);
   console.log(arr);
-}, errorHendler);
+}, errorHendler,
+ '=male');
 
 requestForUsers(function(female){
   arr2.push(female);
   console.log(arr2);
-}, errorHendler);
+}, errorHendler,
+'=female');
 
-//console.log(JSON.parse(xhr.response));
