9cbb92786352ab007a5584e9b55ffa01473bf451
EdwardHushaleu <2779522@mail.com>
2020-06-19 23:23:37 +0300

    Finding an element in an array of objects

--- 10_homework15.6.20_20.6.20/script.js
+++ 10_homework15.6.20_20.6.20/script.js
@@ -1,11 +1,15 @@
 'use strict'
-const errorHendler = (errorE) => {console.log(errorE.message)};
-let arr =[];
-let arr2 =[];
+const errorHendler = (errorE) => {
+  console.log(
+    errorE.cod,
+    errorE.message
+    )
+};
+
 const requestForUsers = function (seccess, error, url){
   const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'https://randomuser.me/api/?results=3&gender' + url);
+  xhr.open('GET', 'https://randomuser.me/api/?results=3&' + url);
   xhr.onreadystatechange = function(){
     if(xhr.readyState === 4){
       if(xhr.status >= 200 && xhr.status < 300) {
@@ -14,7 +18,8 @@ const requestForUsers = function (seccess, error, url){
 
       } else{
           error({
-            message: 'Error',
+            cod: xhr.status,
+            message: xhr.responseText,
           });
       }
     }
@@ -23,15 +28,28 @@ const requestForUsers = function (seccess, error, url){
   xhr.send();
 };
 
+
+
+
 requestForUsers(function(male){
-  arr.push(male);
-  console.log(arr);
+  console.log('male', male.results);
+  let maleArr = male.results;
+  
+  maleArr.forEach(function(item){
+      console.log(item.name.first)
+  });
+ 
 }, errorHendler,
  '=male');
 
 requestForUsers(function(female){
-  arr2.push(female);
-  console.log(arr2);
+  console.log('female',female.results);
+  let femaleArr = female.results;
+
+  femaleArr.forEach(function(item){
+    console.log(item.name.first)
+  })
+  
 }, errorHendler,
 '=female');
 
