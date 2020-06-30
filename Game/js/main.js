468b8775c317d5feba89411c7ec91bd20ca26147
EdwardHushaleu <2779522@mail.com>
2020-06-30 23:07:16 +0300

    Added task for the game 2 and 3

--- Game/js/main.js
+++ Game/js/main.js
@@ -2,6 +2,13 @@ var body = document.body
 var bShooter = document.querySelector('.b-shooter');
 var bShooterAim = document.querySelector('.b-shooter__aim');
 var bShooterImgAim = document.querySelector('.b-shooter__img-aim');
+var ghost = document.querySelector('.b-shooter__img-ghost');
+var bShooterImgFire = document.querySelector('.b-shooter__img-fire')
+var delayToReset = 500;
+//Анимация в момент поподания
+var animation = `opacity: 0;
+transition-duration: ${delayToReset * 0.6}ms;
+transition-delay: ${delayToReset * 0.4}ms;`;
 
 bShooter.addEventListener('click', function(e){
      var x = e.offsetX - bShooterAim.offsetWidth / 2;
@@ -36,7 +43,75 @@ body.addEventListener('keydown', function(e){
 body.addEventListener('keyup', function(e){
     
     if(e.keyCode === 13){
+        //находим координаты методом getBoundingClientRect() у элемента bShooterImgAim
+        var coordShooter = bShooterImgAim.getBoundingClientRect();
+
+        //вычислем полученые координаты, aimCenterX и aimCenterY, которые будут являться центром изображения прицела.
+        var aimCenterX = coordShooter.x + coordShooter.width / 2;
+        var aimCenterY = coordShooter.y + coordShooter.height / 2;
+        
+        // с помощю метода getBoundingClientRect() берем значения left, top, right и bottom для Hitbox .
+        var coordGhost = ghost.getBoundingClientRect();
+
         bShooterImgAim.style.transform = '';  
+
+        //проверка, попадают ли координаты геометрического центра прицела в наш хитбокс
+        if(aimCenterX < coordGhost.right - 20
+           && aimCenterX > coordGhost.left + 20
+           && aimCenterY > coordGhost.top - 20
+           && aimCenterY < coordGhost.bottom + 20
+           ){
+            
+            
+            //В момент попадания в цель появляется изображение огня 
+            bShooterImgFire.style.visibility ='visible';
+            //задаётся длительность анимации, задержка изображение огня и opacity 0
+            bShooterImgFire.style.cssText = bShooterImgFire.style.cssText + animation;
+            //тоже задаётся длительность анимации, задержка и opacity 0
+            ghost.style.cssText = ghost.style.cssText + animation;
+            // скрываем прицел после попадания 
+            bShooterImgAim.style.display = 'none';
+
+            // после запуска анимации Удаляем все стили после регистрации setTimeout время отложенного запуска которого будет равняться delayToReset 
+            setTimeout(function(){
+                bShooterImgFire.removeAttribute('style');
+                ghost.removeAttribute('style');
+                bShooterImgAim.style.display = '';
+                bShooterImgFire.style.visibility = 'hidden';
+            
+            },delayToReset)
+        }
     }
     
 });
+
+function setRandomCoords(){
+    //оперируем размерами игровой области и размерами картинки с приведением                                                 
+    var limitX = bShooter.offsetWidth - ghost.offsetWidth;
+    var limitY = bShooter.offsetHeight - ghost.offsetHeight;
+
+    //генерируем случайную пару x, y допустимых координат
+    var x = Math.floor(Math.random() * (limitX + 1));
+    var y = Math.floor(Math.random() * (limitY + 1));
+
+    //полученные координаты задаем как top и left для .b-shooter__img-ghost
+    ghost.style.top = y + 'px';
+    ghost.style.left = x + 'px';
+}
+
+//реализовываем расписание появления приведения с помощью setInterval в 3 секунды
+setInterval(function(){
+
+    //проверка стилевого правила display у картинки .b-shooter__img-ghost
+    if(ghost.style.display === 'none'){
+        ghost.style.display = '';
+    };
+    
+    /*что бы призрак при удалении не перемещался по случайным координатам добавляем проверку на наличие
+        свойств анимации у .b-shooter__img-ghost. Если свойство есть, то запуск setRandomCoords происходить не должен.*/
+    if(!ghost.style.animation){
+        //безусловный вызов setRandomCoords
+        setRandomCoords();
+    }
+}, 3000);
+
